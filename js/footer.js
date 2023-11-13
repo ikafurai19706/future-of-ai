c406367dd5193eb013f588beb5c90312465cd4f5
ikafurai19706 <ikafurai19706@gmail.com>
2023-11-13 21:58:04 +0900

    Add footer.js & remove old footer

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+f.innerHTML =
+  `<div class="bar"></div>
+  <ul class="menu">
+    <li>
+      <a href="javascript:link('index.html')" class="foo">ホーム</a>
+    </li>
+    <li>
+      <a href="javascript:link('sitemap.html')" class="foo">サイトマップ</a>
+    </li>
+    <li>
+      <a class="foo">工事中</a>
+    </li>
+    <li>
+      <a class="foo">工事中</a>
+    </li>
+  </ul>
+  <p>©1192 FoAI team. ALL Rights Reserved.</p>`;
